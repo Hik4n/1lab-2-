c13396d7141f241528b150683ae054bf0fa825ea
Hik4n <145261618+Hik4n@users.noreply.github.com>
2025-04-07 19:44:36 +0300

    Add files via upload

---
+++ ViewModels/QueueViewModel.cs
@@ -0,0 +1,51 @@
+﻿using QueueApp.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace QueueApp.ViewModels
+{
+    public class QueueViewModel : INotifyPropertyChanged
+    {
+        private Queue<string> queue;
+
+        public QueueViewModel()
+        {
+            queue = new Queue<string>();
+        }
+
+        public string? CurrentItem => queue.Count > 0 ? queue.CurrentItem : null; 
+        public int Count => queue.Count;
+        public bool IsEmpty => queue.IsEmpty;
+
+        public void Enqueue(string item)
+        {
+            queue.Enqueue(item);
+            OnPropertyChanged(nameof(CurrentItem));
+            OnPropertyChanged(nameof(Count));
+            OnPropertyChanged(nameof(IsEmpty));
+        }
+
+        public string Dequeue()
+        {
+            string item = queue.Dequeue();
+            OnPropertyChanged(nameof(CurrentItem));
+            OnPropertyChanged(nameof(Count));
+            OnPropertyChanged(nameof(IsEmpty));
+            return item;
+        }
+
+        public void Clear()
+        {
+            queue.Clear();
+            OnPropertyChanged(nameof(CurrentItem));
+            OnPropertyChanged(nameof(Count));
+            OnPropertyChanged(nameof(IsEmpty));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null!)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
